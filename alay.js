9564b40b3d3390bd0c4700ce76e4ea751fbbbda4
RzeL <62789747+RzeL-TKJ@users.noreply.github.com>
2020-07-01 01:03:33 +0700

    Added alay.js
    The Main Source Of This Project.

---
+++ alay.js
@@ -0,0 +1,79 @@
+function encrypt() {
+    var main   = "abgsezi";
+    var decode = "4695321";
+
+    main = main.split("");
+    decode = decode.split("");
+
+    var input = prompt("Write The Text Here.");
+
+    input = input.toLowerCase();
+
+    var output = "";
+
+    var x;
+    var a;
+
+    for (x of input) {
+        var b = main.indexOf(x);
+        
+        if (b == -1) {
+            a = Math.floor(Math.random() * 2);
+            
+            if (a == 1) {
+                x = x.toUpperCase();
+                output = output+x;
+            } else {
+                output = output+x;
+            };
+        } else {
+            a = Math.floor(Math.random() * 4);
+            
+            if (a == 3) {
+                a = Math.floor(Math.random() * 2);
+            
+                if (a == 1) {
+                    x = x.toUpperCase();
+                    output = output+x;
+                } else {
+                    output = output+x;
+                };
+				
+            } else {
+				output = output+decode[b];
+            };
+            
+        };
+    };
+
+    console.log(output)
+};
+
+function decrypt() {
+    var main   = "abgsezi";
+    var decode = "4695321";
+
+    main = main.split("");
+    decode = decode.split("");
+
+    var input = prompt("Write The Text Here.");
+
+    input = input.toLowerCase();
+
+    var output = "";
+
+    var x;
+    var a;
+
+    for (x of input) {
+        var b = decode.indexOf(x);
+        
+        if (b == -1) {
+            output = output+x;
+        } else {
+            output = output+main[b];
+        };
+    };
+
+    console.log(output)
+};
